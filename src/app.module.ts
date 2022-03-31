8b14655fd2e36613bc892662eb96bc0aa7202501
ppodds <oscar20020629@gmail.com>
2022-04-01 00:38:25 +0800

    Feat: remove unneed files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UrlModule } from './url/url.module';
 import configuration from './config/configuration';
 import { Url } from './url/url.entity';
@@ -20,7 +18,5 @@ import { Url } from './url/url.entity';
     }),
     UrlModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
