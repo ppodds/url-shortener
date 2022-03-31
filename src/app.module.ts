8a7380bf432215e52fa31ecb342b685b07d8a948
ppodds <oscar20020629@gmail.com>
2022-03-31 19:54:49 +0800

    Feat: setup db and env files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [configuration] }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (config: ConfigService) =>
+        ({
+          ...config.get('database'),
+        } as TypeOrmModuleOptions),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/configuration.ts
@@ -0,0 +1,13 @@
+export default () => ({
+  database: {
+    type: process.env.DB_TYPE || 'mysql',
+    host: process.env.DB_HOST || '127.0.0.1',
+    port: process.env.DB_PORT || 3306,
+    username: process.env.DB_USER || 'username',
+    password: process.env.DB_PASSWORD || 'password',
+    database: process.env.DB_NAME || 'dbname',
+    extra: {
+      connectionLimit: 10,
+    },
+  },
+});
