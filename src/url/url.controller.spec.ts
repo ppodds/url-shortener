a1a81e5ca3e88c68686bed66efd647e2472f56be
ppodds <oscar20020629@gmail.com>
2022-03-31 23:56:55 +0800

    Test: add test for url controller

--- src/url/url.controller.spec.ts
+++ src/url/url.controller.spec.ts
@@ -1,19 +1,24 @@
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigService } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
-import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { Response } from 'express';
+import { async } from 'rxjs';
 import { Repository } from 'typeorm';
-import configuration from '../config/configuration';
+import { CreateUrlDto } from './dto/create-url.dto';
 import { UrlController } from './url.controller';
 import { Url } from './url.entity';
 import { UrlService } from './url.service';
 
 describe('UrlController', () => {
   let controller: UrlController;
+  let service: UrlService;
+  const createUrlDto = {
+    url: 'https://www.google.com',
+    expireAt: new Date(Date.now() + 60 * 60 * 1000).toISOString(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UrlController],
-      providers: [UrlService],
     })
       .useMocker((token) => {
         if (token === ConfigService) {
@@ -24,14 +29,39 @@ describe('UrlController', () => {
           };
         } else if (token === 'UrlRepository') {
           return {} as Repository<Url>;
+        } else if (token === UrlService) {
+          return {
+            createUrl: jest
+              .fn()
+              .mockImplementation(async (createUrlDto: CreateUrlDto) => {
+                return {
+                  id: '1',
+                  shortUrl: 'http://localhost:3000/1',
+                };
+              }),
+            getUrl: jest.fn().mockImplementation(async (id: string) => {
+              return 'https://www.google.com';
+            }),
+          };
         }
       })
       .compile();
 
     controller = module.get<UrlController>(UrlController);
+    service = module.get<UrlService>(UrlService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+  it('should call createUrl', async () => {
+    await controller.createUrl(createUrlDto);
+    expect(service.createUrl).toHaveBeenCalledWith(createUrlDto);
+  });
+  it('should call getUrl', async () => {
+    await controller.toUrl('1', {
+      redirect: jest.fn(),
+    } as unknown as Response);
+    expect(service.getUrl).toHaveBeenCalledWith('1');
   });
 });
